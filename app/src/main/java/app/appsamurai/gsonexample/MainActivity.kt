85314b71c52ddcc55d6fd59d0505739b34b644b2
ukyo <ukyo99999@gmail.com>
2019-07-10 23:31:27 +0800

    Add volley as http request library

--- app/src/main/java/app/appsamurai/gsonexample/MainActivity.kt
+++ app/src/main/java/app/appsamurai/gsonexample/MainActivity.kt
@@ -1,12 +1,31 @@
 package app.appsamurai.gsonexample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://jsonplaceholder.typicode.com/users"
+
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener { response ->
+                Log.i("response", response.toString())
+            },
+            Response.ErrorListener { error ->
+                Log.e("ErrorListener", error.toString())
+            })
+
+        queue.add(stringRequest)
     }
+
 }
